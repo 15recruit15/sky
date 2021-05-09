fbc66a693cccee2a8d0eb77d0b3d32c4204fb78a
15recruit15 <15recruit15@gmail.com>
2021-05-09 07:18:04 +0000

    add explanation to application.css.scss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,4 +63,11 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user.admin?
     end 
     
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインして下さい"
+        redirect_to login_url
+      end
+    end
+    
 end
