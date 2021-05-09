dfce79000f4f5739099146ffd754cb0dd189c0df
15recruit15 <15recruit15@gmail.com>
2021-05-09 17:24:09 +0000

    make seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([
+  {  
+    name: "Master-Pilot",
+    email: "123456@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    admin: true
+  },{
+    name: "Darkness-Pilot",
+    email: "1111111@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },{
+    name: "さすらいの訓練生",
+    email: "222222@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },{
+    name: "しゃかりきパイロット",
+    email: "333333@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },{
+    name: "三流操縦士",
+    email: "444444@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },{
+    name: "ボーボボ",
+    email: "555555@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  }
+])
+  
+  
