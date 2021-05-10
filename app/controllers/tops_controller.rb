03e7f152f5bbc9d63b364185d09c3072545628bc
15recruit15 <15recruit15@gmail.com>
2021-05-10 11:44:02 +0000

    kaminari with bootstrap4

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,11 +1,11 @@
 class TopsController < ApplicationController
   def home
-    @questions = Question.all.page(params[:page]).per(10) 
+    @questions = Question.all.page(params[:page]).per(6) 
   end
   
   def search
-    @questions_searched = Question.search(params[:search]).page(params[:page]).per(10)
-    @questions = Question.all.page(params[:page]).per(10) 
+    @questions_searched = Question.search(params[:search]).page(params[:page]).per(6)
+    @questions = Question.all.page(params[:page]).per(6) 
     render 'home'
   end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @questions = @user.questions.page(params[:page]).per(10)
+    @questions = @user.questions.page(params[:page]).per(6)
   end
   
   def new
