08d0513d2b92c7b0f33d83247a6c53af68c8f092
15recruit15 <15recruit15@gmail.com>
2021-05-12 17:25:03 +0000

    docker for production

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,10 +1,6 @@
- $worker  = 2
+  $worker  = 2
   $timeout = 30
-  if Rails.env.development? || Rails.env.test?
-    $app_dir = "/myproject" 
-  elsif Rails.env.production?
-    $app_dir = "/var/www/rails/sample" 
-  end 
+  $app_dir = "/myproject" 
   $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
